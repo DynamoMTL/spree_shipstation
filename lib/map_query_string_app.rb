f052f5ce8d648c7a4340e58dff321c6812793530
Josh Nussbaum <joshnuss@gmail.com>
2013-02-28 17:00:28 -0500

    updates error trapping in rack app

--- lib/map_query_string_app.rb
+++ lib/map_query_string_app.rb
@@ -8,11 +8,7 @@ class MapQueryStringApp
     query           = Rack::Utils.parse_nested_query(env['QUERY_STRING'])
     action          = query['action'] || :index
 
-    begin
-      controller = controller_name.classify.constantize
-      controller.action(action).call(env)
-    rescue NameError
-      raise "#{controller_name} is an invalid parameter"
-    end
+    controller = controller_name.classify.constantize
+    controller.action(action).call(env)
   end
 end
