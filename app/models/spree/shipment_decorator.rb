87fb03f350cb8bdbcb05dcd6e75ac5d20e2cd16a
Josh Nussbaum <joshnuss@gmail.com>
2013-02-28 16:45:07 -0500

    adds Shipment#exportable scope

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,4 +1,6 @@
 Spree::Shipment.class_eval do
+  scope :exportable, where('state != ?', 'pending')
+
   def self.between(from, to)
     where('updated_at between ? and ?', from, to)
   end
--- spec/models/spree/shipment_spec.rb
+++ spec/models/spree/shipment_spec.rb
@@ -5,11 +5,11 @@ describe Spree::Shipment do
     before do
       @active = []
 
-      Factory.create(:shipment, updated_at: Time.now - 1.days)
-      Factory.create(:shipment, updated_at: Time.now + 1.days)
+      create_shipment(updated_at: Time.now - 1.days)
+      create_shipment(updated_at: Time.now + 1.days)
 
-      @active << Factory.create(:shipment, updated_at: Time.now)
-      @active << Factory.create(:shipment, updated_at: Time.now)
+      @active << create_shipment(updated_at: Time.now)
+      @active << create_shipment(updated_at: Time.now)
     end
 
     subject { Spree::Shipment.between(Time.now-1.hour, Time.now + 1.hours) }
@@ -17,5 +17,26 @@ describe Spree::Shipment do
     specify { should have(2).shipment }
 
     specify { should == @active }
+  end
+
+  context "exportable" do
+    let!(:pending) { create_shipment(state: 'pending') }
+    let!(:ready)   { create_shipment(state: 'ready')   }
+    let!(:shipped) { create_shipment(state: 'shipped') }
+
+    subject { Spree::Shipment.exportable }
+
+    specify { should have(2).shipments }
+
+    specify { should include(ready)}
+    specify { should include(shipped)}
+
+    specify { should_not include(pending)}
+  end
+
+  def create_shipment(options={})
+    Factory.create(:shipment, options).tap do |shipment|
+      shipment.update_column(:state, options[:state]) if options[:state]
+    end
   end
 end
