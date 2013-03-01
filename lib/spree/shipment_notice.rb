d9c2c851697f1bf6dbc3f23cc07112d5f090bf87
Josh Nussbaum <joshnuss@gmail.com>
2013-03-01 11:12:01 -0500

    adds logic to apply tracking number

--- lib/spree/shipment_notice.rb
+++ lib/spree/shipment_notice.rb
@@ -1,4 +1,21 @@
 module Spree
   class ShipmentNotice
+    attr_reader :error
+
+    def initialize(params) 
+      @number   = params[:order_number]
+      @tracking = params[:tracking_number]
+
+      @shipment = Shipment.find_by_number(@number)
+    end
+
+    def apply
+      if @shipment
+        @shipment.update_attribute(:tracking, @tracking)
+        @shipment.ship! if @shipment.can_ship?
+
+        true
+      end
+    end
   end
 end
---
+++ spec/lib/spree/shipment_notice_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+include Spree
+
+describe Spree::ShipmentNotice do
+  let(:notice) { ShipmentNotice.new(order_number:    'S12345', 
+                                    tracking_number: '1Z1231234') }
+
+  context "#apply" do
+    context "shipment found" do
+      let(:shipment) { mock_model(Shipment, can_ship?: true) }
+
+      before do
+        Shipment.should_receive(:find_by_number).with('S12345').and_return(shipment)
+        shipment.should_receive(:update_attribute).with(:tracking, '1Z1231234')
+        shipment.should_receive(:ship!)
+      end
+
+      specify { notice.apply.should be_true }
+    end
+
+    context "shipment not found" do
+      before do
+        Shipment.should_receive(:find_by_number).with('S12345').and_return(nil)
+      end
+
+      specify { notice.apply.should be_false }
+    end
+
+    context "shipment already shipped" do
+      let(:shipment) { mock_model(Shipment, 'can_ship?' => false) }
+
+      before do
+        Shipment.should_receive(:find_by_number).with('S12345').and_return(shipment)
+        shipment.should_receive(:update_attribute).with(:tracking, '1Z1231234')
+        shipment.should_not_receive(:ship!)
+      end
+
+      specify { notice.apply.should be_true }
+    end
+  end
+end
