edfcbf100bf1b52618da70992ace8aaf8b896c12
Josh Nussbaum <joshnuss@gmail.com>
2013-03-01 12:33:56 -0500

    refactors ShipmentNotice class

--- lib/spree/shipment_notice.rb
+++ lib/spree/shipment_notice.rb
@@ -5,23 +5,34 @@ module Spree
     def initialize(params) 
       @number   = params[:order_number]
       @tracking = params[:tracking_number]
-
-      @shipment = Shipment.find_by_number(@number)
     end
 
     def apply
-      if @shipment
-        @shipment.update_attribute(:tracking, @tracking)
-        @shipment.ship! if @shipment.can_ship?
-
-        true
-      else
-        @error = I18n.t(:shipment_not_found, number: @number)
-        false
-      end
+      locate ? update : not_found
     rescue => e
-      Rails.logger.error(e)
-      @error = I18n.t(:import_tracking_error, error: e.to_s)
+      handle_error(e)
+    end
+
+  private
+    def locate
+      @shipment = Shipment.find_by_number(@number)
+    end
+
+    def update
+      @shipment.update_attribute(:tracking, @tracking)
+      @shipment.ship! if @shipment.can_ship?
+
+      true
+    end
+
+    def not_found
+      @error = I18n.t(:shipment_not_found, number: @number)
+      false
+    end
+    
+    def handle_error(error)
+      Rails.logger.error(error)
+      @error = I18n.t(:import_tracking_error, error: error.to_s)
       false
     end
   end
