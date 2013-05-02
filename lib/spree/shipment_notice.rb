e26a0283b254dcfbf6a5f3edabe6e557043b8fa1
Jonathan Neddenriep <river1@gmail.com>
2013-05-02 14:22:45 -0700

    Added line item shipping.

--- lib/spree/shipment_notice.rb
+++ lib/spree/shipment_notice.rb
@@ -20,8 +20,12 @@ module Spree
 
     def update
       @shipment.update_attribute(:tracking, @tracking)
-      @shipment.reload.update_attribute(:state, 'shipped') unless @shipment.shipped?
 
+      unless @shipment.shipped?
+        @shipment.reload.update_attribute(:state, 'shipped')
+        @shipment.inventory_units.each &:ship!
+        @shipment.touch :shipped_at
+      end
       true
     end
 
