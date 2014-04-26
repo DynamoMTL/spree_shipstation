acd9d39e115a07d43e37fe1225ee9df0422f79da
Bryan Mahoney <bryan@goDynamo.com>
2014-04-26 07:46:26 -0400

    Quick support to filter out domestic orders from shipstation

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -5,6 +5,10 @@ Spree::Shipment.class_eval do
     joins(:order).where('(spree_shipments.updated_at > ? AND spree_shipments.updated_at < ?) OR (spree_orders.updated_at > ? AND spree_orders.updated_at < ?)',from, to, from, to)
   end
 
+  def self.international
+    where('spree_addresses.country_id = ?', ::Spree::Country.find_by_name('Canada').id).joins(:address)
+  end
+
 private
   def send_shipped_email
     Spree::ShipmentMailer.shipped_email(self).deliver if Spree::Config.send_shipped_email
